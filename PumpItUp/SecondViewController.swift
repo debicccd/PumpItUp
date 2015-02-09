03ca5657ee71748b8db5d58aa7cc49551b4c525f
debicccd <debicccd@rose-hulman.edu>
2015-02-09 05:29:33 -0500

    Initial Commit

---
+++ PumpItUp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PumpItUp
+//
+//  Created by CSSE Department on 2/9/15.
+//  Copyright (c) 2015 CSSE Department. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
